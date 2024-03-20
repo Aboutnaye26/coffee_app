26dc6008a5a3e311328eea08693257540e3bfd5f
Abraham Esandayinze Tanta <abraham@tantainnovatives.com>
2024-03-20 11:48:29 +0100

    Created the Menu Function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# testing
+import time
+
+def display_menu():
+    print("Welcome to our Coffee Shop!")
+    print("Please choose your coffee:")
+    print("1. Americano - $3.00")
+    print("2. Latte - $3.50")
+    print("3. Cappuccino - $4.00")
+    print("4. Espresso - $2.50")
+    print("5. Finish order")
+
